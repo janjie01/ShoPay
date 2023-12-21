46cb9eba54c8734223f14de994981e939f48ff8c
Janjie Aljas <janjie.aljas1@gmail.com>
2023-12-22 04:18:53 +0800

    Updated home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Card,
-  Button,
-  Container,
-  Row,
-  Col,
-  Navbar,
-  Nav,
-} from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button, Container, Row, Col, Navbar, Nav, Form } from 'react-bootstrap';
 
 function Dashboard() {
   const navigate = useNavigate();
   const [productData, setProductData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch(`https://shopay-t848.onrender.com/product`)
@@ -22,86 +15,71 @@ function Dashboard() {
         setProductData(responseData);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
       });
   }, []);
 
   const handleLogout = () => {
     fetch(`https://shopay-t848.onrender.com/logout`, {
-      method: "POST",
-      credentials: "include",
+      method: 'POST',
+      credentials: 'include',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => response.json())
       .then((data) => {
-        if (data.Status === "Success") {
-          console.log("Logout Successfully");
-          navigate("/");
+        if (data.Status === 'Success') {
+          console.log('Logout Successfully');
+          navigate('/');
         } else {
-          console.error("Logout failed");
+          console.error('Logout failed');
         }
       })
       .catch((error) => {
-        console.error("Error during logout:", error);
+        console.error('Error during logout:', error);
       });
   };
 
+  // Filter products based on search query
+  const filteredProducts = productData.filter(
+    (product) =>
+      product.product_name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      product.product_description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
-      <div>
-        <Navbar
-          bg="success"
-          variant="dark"
-          expand="lg"
-          fixed="top"
-          className="p-2"
-        >
-          <Navbar.Brand href="/">
-            <strong>ShoPay</strong>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto d-flex align-items-center">
-              <Button variant="light" href="/login">
-                Login
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+      <Navbar bg="success" variant="dark" expand="lg" fixed="top" className="p-2 justify-content-between">
+        <Navbar.Brand href="/"><strong>ShoPay</strong></Navbar.Brand>
+        <Form inline className="mx-auto">
+          <Form.Control
+            type="text"
+            placeholder="Search Products"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </Form>
+        <Nav>
+          <Button variant="light" href="/login">
+            Login
+          </Button>
+        </Nav>
+      </Navbar>
+
       <Container fluid className="mt-5 pt-5">
         <Row xs={2} md={3} lg={4} xl={5} xxl={6} className="g-4">
-          {productData.map((product, index) => (
+          {filteredProducts.map((product, index) => (
             <Col key={index}>
-              <Card
-                style={{
-                  width: "12rem",
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <Card.Img
-                  variant="top"
-                  src={product.product_photo}
-                  style={{ height: "50%", objectFit: "cover" }}
-                />
-                <Card.Text>Price: {product.product_price}</Card.Text>
-                <Card.Text>Available Quantity: {product.product_qty}</Card.Text>
+              <Card style={{ width: '12rem', height: '100%', display: 'flex', flexDirection: 'column' }}>
+                <Card.Img variant="top" src={product.product_photo} style={{ height: '50%', objectFit: 'cover' }} />
                 <Card.Body className="d-flex flex-column">
                   <Card.Title>{product.product_name}</Card.Title>
                   <Card.Text>{product.product_description}</Card.Text>
                   <div className="mt-auto d-sm-inline-block">
-                    <Card.Text>
-                      Available Quantity: {product.product_qty}
-                    </Card.Text>
-                    <Button
-                      as={Link}
-                      to={`/product/${product.product_id}`}
-                      variant="success"
-                    >
+                    <Card.Text>Price: {product.product_price}</Card.Text>
+                    <Card.Text>Available Quantity: {product.product_qty}</Card.Text>
+                    <Button as={Link} to={`/product/${product.product_id}`} variant="success">
                       View Details
                     </Button>
                   </div>
