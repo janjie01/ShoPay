032885c0d64e2656155470e174f255bc123aca4c
Janjie Aljas <janjie.aljas1@gmail.com>
2023-12-21 20:15:23 +0800

    removed hashing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express, { response } from 'express';
 import mysql from 'mysql';
-import cors from 'cors'; 
-import bcrypt from 'bcrypt';
+import cors from 'cors';
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser';
@@ -66,71 +65,64 @@ db.connect((err) => {
         console.log('Connected to the database');
     }
 });
-
 // Registration
 app.post('/register', (req, res) => {
-    const checkEmailQuery = "SELECT * FROM users WHERE email = ?";
-    const checkUsernameQuery = "SELECT * FROM users WHERE username = ?";
-    const insertUserQuery = "INSERT INTO users (`name`, `username`, `birthdate`, `address`, `role`, `email`, `profile_pic`, `password`) VALUES (?)";
+  const checkEmailQuery = "SELECT * FROM users WHERE email = ?";
+  const checkUsernameQuery = "SELECT * FROM users WHERE username = ?";
+  const insertUserQuery = "INSERT INTO users (`name`, `username`, `birthdate`, `address`, `role`, `email`, `profile_pic`, `password`) VALUES (?)";
 
-    // Check if the email already exists
-    db.query(checkEmailQuery, [req.body.email], (errEmail, resultEmail) => {
-        if (errEmail) {
-            console.error("Error checking email:", errEmail);
-            return res.status(500).json({ Error: "Internal Server Error" });
-        }
+  // Check if the email already exists
+  db.query(checkEmailQuery, [req.body.email], (errEmail, resultEmail) => {
+      if (errEmail) {
+          console.error("Error checking email:", errEmail);
+          return res.status(500).json({ Error: "Internal Server Error" });
+      }
 
-        if (resultEmail.length > 0) {
-            return res.json({ Status: "Email already exists" });
-        }
+      if (resultEmail.length > 0) {
+          return res.json({ Status: "Email already exists" });
+      }
 
-        // Check if the username already exists
-        db.query(checkUsernameQuery, [req.body.username], (errUsername, resultUsername) => {
-            if (errUsername) {
-                console.error("Error checking username:", errUsername);
-                return res.status(500).json({ Error: "Internal Server Error" });
-            }
+      // Check if the username already exists
+      db.query(checkUsernameQuery, [req.body.username], (errUsername, resultUsername) => {
+          if (errUsername) {
+              console.error("Error checking username:", errUsername);
+              return res.status(500).json({ Error: "Internal Server Error" });
+          }
 
-            if (resultUsername.length > 0) {
-                return res.json({ Status: "Username already exists" });
-            }
+          if (resultUsername.length > 0) {
+              return res.json({ Status: "Username already exists" });
+          }
 
-            // Hash the password and insert the user into the database
-            bcrypt.hash(req.body.password.toString(), salt, (hashErr, hash) => {
-                if (hashErr) {
-                    console.error("Error hashing password:", hashErr);
-                    return res.status(500).json({ Error: "Internal Server Error" });
-                }
+          // Insert the user into the database with plain text password
+          const values = [
+              req.body.name,
+              req.body.username,
+              req.body.birthdate,
+              req.body.address,
+              req.body.role,
+              req.body.email,
+              req.body.profile,
+              req.body.password  // Store plaintext password directly
+          ];
 
-                const values = [
-                    req.body.name,
-                    req.body.username,
-                    req.body.birthdate,
-                    req.body.address,
-                    req.body.role,
-                    req.body.email,
-                    req.body.profile,
-                    hash
-                ];
+          if (db.state === 'disconnected') {
+            db.connect();
+          }
 
-                if (db.state === 'disconnected') {
-                  db.connect();
-                }
+          // Insert the user into the database
+          db.query(insertUserQuery, [values], (insertErr, insertResult) => {
+              if (insertErr) {
+                  console.error("Error inserting user:", insertErr);
+                  return res.status(500).json({ Error: "Internal Server Error" });
+              }
 
-                // Insert the user into the database
-                db.query(insertUserQuery, [values], (insertErr, insertResult) => {
-                    if (insertErr) {
-                        console.error("Error inserting user:", insertErr);
-                        return res.status(500).json({ Error: "Internal Server Error" });
-                    }
-
-                    return res.json({ Status: "Success" });
-                });
-            });
-        });
-    });
+              return res.json({ Status: "Success" });
+          });
+      });
+  });
 });
-//Login
+
+// Login
 app.post('/login', (req, res) => {
   const sql = 'SELECT * FROM users WHERE email = ?';
   db.query(sql, [req.body.email], (err, data) => {
@@ -141,44 +133,42 @@ app.post('/login', (req, res) => {
       }
 
       if (data.length > 0) {
-          bcrypt.compare(req.body.password.toString(), data[0].password, (err, response) => {
-              if (err) return res.json({ Error: "Password compare error" });
+          // Compare plaintext passwords directly
+          if (req.body.password === data[0].password) {
+              const name = data[0].name;
+              const userId = data[0].user_id; // Retrieve the user ID
+              const userRole = data[0].role;
 
-              if (response) {
-                  const name = data[0].name;
-                  const userId = data[0].user_id; // Retrieve the user ID
-                  const userRole = data[0].role;
-
-                  // Set the appropriate secret key based on the user role
-                  let secretKey;
-                  if (userRole === 'admin') {
-                    if (!process.env.ADMIN_TOKEN) {
-                        return res.json({ Error: "Admin token not configured" });
-                    }
-                    secretKey = process.env.ADMIN_TOKEN;
-                  } else if (userRole === 'user') {
-                      if (!process.env.USER_TOKEN) {
-                          return res.json({ Error: "User token not configured" });
-                      }
-                      secretKey = process.env.USER_TOKEN;
-                  } else {
-                      return res.json({ Error: "Invalid user role" });
+              // Set the appropriate secret key based on the user role
+              let secretKey;
+              if (userRole === 'admin') {
+                if (!process.env.ADMIN_TOKEN) {
+                    return res.json({ Error: "Admin token not configured" });
+                }
+                secretKey = process.env.ADMIN_TOKEN;
+              } else if (userRole === 'user') {
+                  if (!process.env.USER_TOKEN) {
+                      return res.json({ Error: "User token not configured" });
                   }
-                  
-                  // Sign the token using the selected secret key
-                  const token = jwt.sign({ userId, name }, secretKey, { expiresIn: '1d' });
-                  res.cookie('token', token);
-
-                  return res.json({ Status: "Success", Role: userRole, UserId: userId });
+                  secretKey = process.env.USER_TOKEN;
               } else {
-                  return res.json({ Error: "Password not matched" });
+                  return res.json({ Error: "Invalid user role" });
               }
-          });
+              
+              // Sign the token using the selected secret key
+              const token = jwt.sign({ userId, name }, secretKey, { expiresIn: '1d' });
+              res.cookie('token', token);
+
+              return res.json({ Status: "Success", Role: userRole, UserId: userId });
+          } else {
+              return res.json({ Error: "Password not matched" });
+          }
       } else {
           return res.json({ Error: "No email existed" });
       }
   });
 });
+
 //Profile and Purchase History
 app.get('/profile', authenticateToken, (req, res) => {
   const userId = req.userId;
